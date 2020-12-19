ee683257689c02627374551e3164d9c12f48627e
nekkan <nxkkdev@gmail.com>
2020-12-19 01:25:36 -0300

    Override equals function in ApplicationVersion.kt.

--- src/main/kotlin/com/nekkan/bloqs/utils/ApplicationVersion.kt
+++ src/main/kotlin/com/nekkan/bloqs/utils/ApplicationVersion.kt
@@ -6,6 +6,10 @@ data class ApplicationVersion(val major: Int, val minor: Int, val patch: Int) {
         return "$major.$minor.$patch"
     }
 
+    override fun equals(other: Any?): Boolean {
+        return other is ApplicationVersion && other.major == major && other.minor == minor && other.patch == patch
+    }
+
     companion object {
         val zero = ApplicationVersion(0, 0, 0)
     }
