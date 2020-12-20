cb77a1cf2fe3090846a26d73416e3134e10cb045
nekkan <nxkkdev@gmail.com>
2020-12-19 23:19:33 -0300

    Add vulkanDeviceQueueCreateInfo function to context.kt.

--- src/main/kotlin/com/nekkan/bloqs/vulkan/context.kt
+++ src/main/kotlin/com/nekkan/bloqs/vulkan/context.kt
@@ -6,8 +6,9 @@ package com.nekkan.bloqs.vulkan
 import com.nekkan.bloqs.context.GlfwContext
 import com.nekkan.bloqs.utils.ApplicationVersion
 import org.lwjgl.glfw.GLFW
-import org.lwjgl.vulkan.VK11
+import org.lwjgl.vulkan.VK11.*
 import org.lwjgl.vulkan.VkApplicationInfo
+import org.lwjgl.vulkan.VkDeviceQueueCreateInfo
 import org.lwjgl.vulkan.VkInstanceCreateInfo
 import kotlin.contracts.ExperimentalContracts
 
@@ -15,7 +16,7 @@ import kotlin.contracts.ExperimentalContracts
  * @return A [Int] Vulkan representation of the receiver [ApplicationVersion].
  */
 inline val ApplicationVersion.vulkanVersion: Int
-    get() = VK11.VK_MAKE_VERSION(major, minor, patch)
+    get() = VK_MAKE_VERSION(major, minor, patch)
 
 /**
  * Apply both [GlfwContext.clientApi] and [GlfwContext.resizable] to the GLFW window hints.
@@ -28,12 +29,12 @@ inline fun GlfwContext.applyHints() {
 /**
  * Returns a new instance of [VkApplicationInfo] allocated with [VkApplicationInfo.calloc] and apply the
  * [transformer] callback. This function is marked to use the [ExperimentalContracts] API to improve the
- * compatibility.
+ * compatibility. The instance must be explicitly freed.
  */
 @OptIn(ExperimentalContracts::class)
 inline fun vulkanApplicationInfo(name: String, transformer: VkApplicationInfo.() -> Unit = {}): VkApplicationInfo {
     return VkApplicationInfo.calloc()
-        .sType(VK11.VK_STRUCTURE_TYPE_APPLICATION_INFO)
+        .sType(VK_STRUCTURE_TYPE_APPLICATION_INFO)
         .pApplicationName(+name)
         .apply(transformer)
 }
@@ -41,7 +42,7 @@ inline fun vulkanApplicationInfo(name: String, transformer: VkApplicationInfo.()
 /**
  * Returns a new instance of [VkInstanceCreateInfo] allocated with [VkInstanceCreateInfo.calloc] and apply the
  * [transformer] callback. This function is marked to use the [ExperimentalContracts] API to improve the
- * compatibility.
+ * compatibility. The instance must be explicitly freed.
  */
 @OptIn(ExperimentalContracts::class)
 inline fun vulkanInstanceCreateInfo(
@@ -49,7 +50,23 @@ inline fun vulkanInstanceCreateInfo(
     transformer: VkInstanceCreateInfo.() -> Unit
 ): VkInstanceCreateInfo {
     return VkInstanceCreateInfo.calloc()
-        .sType(VK11.VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO)
+        .sType(VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO)
         .pApplicationInfo(applicationInfo)
+        .apply(transformer)
+}
+
+/**
+ * Returns a new instance of [VkDeviceQueueCreateInfo] allocated with [VkDeviceQueueCreateInfo.calloc] and
+ * apply the [transformer] callback. This function is marked to use the [ExperimentalContracts] API to improve
+ * the compatibility. The instance must be explicitly freed.
+ */
+@OptIn(ExperimentalContracts::class)
+inline fun vulkanDeviceQueueCreateInfo(
+    queueFamilyIndex: Int,
+    transformer: VkDeviceQueueCreateInfo.() -> Unit = {}
+): VkDeviceQueueCreateInfo {
+    return VkDeviceQueueCreateInfo.calloc()
+        .sType(VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO)
+        .queueFamilyIndex(queueFamilyIndex)
         .apply(transformer)
 }
